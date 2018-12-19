548a4973b26115015c8296d88eedef559301b7a1
Michael Randall <m_ran66@hotmail.com>
2018-12-18 20:11:20 -0800

    add ball_cage - list of numbers from 1 to 49 and add draw method that generates win numbers and draw numbers

--- app.py
+++ app.py
@@ -1,8 +1,20 @@
 #!/usr/bin/env python
+import random
+
 
 # generate a lotto ball holder and populate it with all the numbers for drawing
-## use a list with a range of numbers from 1 to 64
-# generate a lotto draw - list 4 random numbers - for wind numbers and draws
+## use a list with a range of numbers from 1 to 49
+
+ball_cage = list(range(1,50))
+print(ball_cage) # test that ball cage contains correct range
+ 
+# generate a lotto draw - list 4 (6) random numbers - for win numbers and draws
+def draw():
+    draw = sorted(random.sample(range(1,50),4))
+    print(draw)
+
+draw() # test draw method
+
 # compare draws to the lotto draw numbers
 # print count, draw number and win number
 # print all draw numbers
