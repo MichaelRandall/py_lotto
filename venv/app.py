bb81f9b83dcdb93c290eeabdbf5871aa7e206bc6
Michael Randall <m_ran66@hotmail.com>
2018-12-18 19:47:56 -0800

    add infrastructure and notes and psuedo code

---
+++ venv/app.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+
+# generate a lotto ball holder and populate it with all the numbers for drawing
+## use a list with a range of numbers from 1 to 64
+# generate a lotto draw - list 4 random numbers - for wind numbers and draws
+# compare draws to the lotto draw numbers
+# print count, draw number and win number
+# print all draw numbers
