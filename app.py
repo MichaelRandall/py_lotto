130d10e85db95cefdcb408bb14e1768d9f615a7b
Michael Randall <m_ran66@hotmail.com>
2018-12-25 21:45:49 -0800

    added functionality for possibility of more than one winning ticket

--- app.py
+++ app.py
@@ -2,6 +2,7 @@
 import random
 import argparse
 
+
 # get user input from command line to determine number of draws to generate
 parser = argparse.ArgumentParser(description='Specify the number of user draws.')
 parser.add_argument('-d','--draw_count', type=int, help='an integer for number of draws')
@@ -10,12 +11,14 @@ draw_count = args.draw_count
 
 
 draw_holder = [] #holds all the draws or user ticket values
+winner_count = 0 #holds the count of all winning tickets - all 6 match
+loser_count = 0 #holds the count of all losing tickets - if not all 6 numbers match, then loser
 
 
 # generate a lotto ball holder and populate it with all the numbers for drawing
 ## use a list with a range of numbers from 1 to 49
 ball_cage = list(range(1,50))
-# print(ball_cage) # test that ball cage contains correct range
+
  
 # generate a lotto draw - list (6) random numbers - for win numbers and draws
 def draw():
@@ -31,11 +34,12 @@ while counter < draw_count:
     counter = counter + 1
 
 
-# compare draws to the lotto draw numbers
+# compare all draws to the lotto draw numbers
 for draw in draw_holder:
     if draw == lotto_draw:
-        print("Win " + str(draw) + " - " + str(lotto_draw))
+        winner_count = winner_count + 1
     else:
-        print("Lose" + str(draw) + " - " + str(lotto_draw))
-# print count, draw number and win number
-# print all draw numbers
+        loser_count = loser_count + 1
+
+# print the results of the drawing
+print(str(winner_count) + ' winning tickets, and ' + str(loser_count) + ' losing tickets.')
