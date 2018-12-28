e9fccd8bebf409aef05108132ce0394ce72a1ced
Michael Randall <m_ran66@hotmail.com>
2018-12-27 23:01:56 -0800

    add ability to draw until a matching set of numbers is drawn

--- app.py
+++ app.py
@@ -1,45 +1,69 @@
 #!/usr/bin/env python
-import random
-import argparse
+import random # used to work with random numbers
+import argparse # used to parse command line arguments
+import locale # used to work with numbers and local formatting
 
 
 # get user input from command line to determine number of draws to generate
 parser = argparse.ArgumentParser(description='Specify the number of user draws.')
 parser.add_argument('-d','--draw_count', type=int, help='an integer for number of draws')
+parser.add_argument('-r','--reverse_game', action="store_true", help="run a game until a winner is drawn")
+parser.add_argument('s','--show_stats', action="store_true", help="display statistics after game")
 args = parser.parse_args()
 draw_count = args.draw_count
-
-
-draw_holder = [] #holds all the draws or user ticket values
-winner_count = 0 #holds the count of all winning tickets - all 6 match
-loser_count = 0 #holds the count of all losing tickets - if not all 6 numbers match, then loser
-
+reverse = args.reverse_game
 
 # generate a lotto ball holder and populate it with all the numbers for drawing
 ## use a list with a range of numbers from 1 to 49
 ball_cage = list(range(1,50))
 
- 
+draw_holder = [] #holds all the draws or user ticket values
+winner_count = 0 #holds the count of all winning tickets - all 6 match
+loser_count = 0 #holds the count of all losing tickets - if not all 6 numbers match, then loser
+
+winner_found = False
+number_of_draws = 0
+
 # generate a lotto draw - list (6) random numbers - for win numbers and draws
+# draw returns a sorted list of 6 random numbers between 1 and 49
 def draw():
-    draw = sorted(random.sample(range(1,50),6))
+    draw = sorted(random.sample(range(1,50),2))
     return draw
 
+
+# draw the lotto numbers because in both instances, the lotto balls have to be drawn
 lotto_draw = draw() # draw the set of numbers that users must match
 
-# need to generate multiple draws and store them for comparison later
-counter = 0
-while counter < draw_count:
-    draw_holder.append(draw())
-    counter = counter + 1
+if reverse:
+    print('Reverse lotto, numbers are drawn until a set of numbers match the lottery numbers')
+    while not winner_found:
+        cur_draw = draw()
+        if cur_draw == lotto_draw:
+            draw_holder.append(cur_draw)
+            winner_found = True
+        else:
+            draw_holder.append(cur_draw)
+            number_of_draws = number_of_draws + 1
+    print(str(number_of_draws))
+else:
+    print('standard lottory game')
+
+    # need to generate multiple draws and store them for comparison later
+    counter = 0
+    while counter < draw_count:
+        draw_holder.append(draw())
+        counter = counter + 1
 
 
-# compare all draws to the lotto draw numbers
-for draw in draw_holder:
-    if draw == lotto_draw:
-        winner_count = winner_count + 1
-    else:
-        loser_count = loser_count + 1
+    # compare all draws to the lotto draw numbers
+    for draw in draw_holder:
+        if draw == lotto_draw:
+            winner_count = winner_count + 1
+        else:
+            loser_count = loser_count + 1
 
-# print the results of the drawing
-print(str(winner_count) + ' winning tickets, and ' + str(loser_count) + ' losing tickets.')
+    # print the results of the drawing
+    print(str(winner_count) + ' winning tickets, and ' + str(loser_count) + ' losing tickets.')
+
+print('Lottery numbers for the week ' + str(lotto_draw))
+#print(draw_holder)
