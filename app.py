61c91c72cd4d2e7f22e35ce30bcd6be7d9f3fc6f
Michael Randall <m_ran66@hotmail.com>
2019-01-03 11:50:26 -0800

    add allow user to specify the number of numbers per draw

--- app.py
+++ app.py
@@ -2,16 +2,21 @@
 import random # used to work with random numbers
 import argparse # used to parse command line arguments
 import locale # used to work with numbers and local formatting
+import itertools # used to flatten a list of lists
+import pandas # used for data analysis - here frequencies
 
 
 # get user input from command line to determine number of draws to generate
 parser = argparse.ArgumentParser(description='Specify the number of user draws.')
 parser.add_argument('-d','--draw_count', type=int, help='an integer for number of draws')
-parser.add_argument('-r','--reverse_game', action="store_true", help="run a game until a winner is drawn")
-parser.add_argument('-s','--show_stats', action="store_true", help="display statistics after game")
+parser.add_argument('-r','--reverse_game', action="store_true", help='run a game until a winner is drawn')
+parser.add_argument('-s','--show_stats', action="store_true", help='display statistics after game')
+parser.add_argument('-n','--number_count', type=int, help='an interger for the number of numbers drawn per lotto.')
 args = parser.parse_args()
 draw_count = args.draw_count
 reverse = args.reverse_game
+stats = args.show_stats
+number_count = args.number_count
 
 # generate a lotto ball holder and populate it with all the numbers for drawing
 ## use a list with a range of numbers from 1 to 49
@@ -27,7 +32,7 @@ number_of_draws = 0
 # generate a lotto draw - list (6) random numbers - for win numbers and draws
 # draw returns a sorted list of 6 random numbers between 1 and 49
 def draw():
-    draw = sorted(random.sample(range(1,50),2))
+    draw = sorted(random.sample(range(1,50),number_count))
     return draw
 
 
@@ -44,9 +49,9 @@ if reverse:
         else:
             draw_holder.append(cur_draw)
             number_of_draws = number_of_draws + 1
-    print(str(number_of_draws))
+    print('Number of draws: ' + str(number_of_draws))
 else:
-    print('standard lottory game')
+    print('Standard lottory game')
 
     # need to generate multiple draws and store them for comparison later
     counter = 0
@@ -65,5 +70,10 @@ else:
     # print the results of the drawing
     print(str(winner_count) + ' winning tickets, and ' + str(loser_count) + ' losing tickets.')
 
-print('Lottery numbers for the week ' + str(lotto_draw))
-#print(draw_holder)
+print('Lottery numbers for the week: ' + str(lotto_draw))
+
+if stats:
+    my_list = list(itertools.chain(*draw_holder))
+    my_series = pandas.Series(my_list)
+    counts = my_series.value_counts()
+    print(str(counts))
